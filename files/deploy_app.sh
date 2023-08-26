c58f35ed19187d4aceeb16ab2d6b3cb71a5be5c1
gidipace <102292792+gidipace@users.noreply.github.com>
2023-08-26 19:13:42 +0200

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,9 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app.
+  Welcome to where time stands still
+  No one leaves and no one will
   <!-- END -->
 
   </div>
