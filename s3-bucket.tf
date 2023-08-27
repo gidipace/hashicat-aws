7aa3223059d238b6ec267ea20b95d1df8546b58e
gidipace <102292792+gidipace@users.noreply.github.com>
2023-08-27 13:02:09 +0200

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,10 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
   bucket_prefix = "giulio"
   bucket = "training-s3-bucket""
   acl    = "private"
-
+  
   versioning = {
     enabled = true
   }
-
 }
